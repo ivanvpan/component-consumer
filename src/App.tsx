517419fe55c447804dfb52d8d8adf809ecc2c3e1
Ivan P <ivanp@fastmail.fm>
2020-01-27 00:25:15 -0700

    Install dep from github

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'component-lib';
 import './App.css';
 
 const App: React.FC = () => {
@@ -19,6 +20,7 @@ const App: React.FC = () => {
           Learn React
         </a>
       </header>
+      <Button onClick={() => console.log('hello')}>Button</Button>
     </div>
   );
 }
